453e58beb7b4fbb015962c3315c3e2153f1a6950
Shesha Sahu <137204935+shesha-sahu@users.noreply.github.com>
2024-01-04 16:26:29 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,7 @@ sudo apt-get -y update
 sudo apt-get -y install ruby
 sudo apt-get -y install wget
 cd /home/ubuntu
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-west-2.s3.amazonaws.com/latest/install
 sudo chmod +x ./install 
 sudo ./install auto
 
@@ -18,4 +18,4 @@ sudo apt-get install -y python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
 
 # delete app
-sudo rm -rf /home/ubuntu/django-aws_cicd
+sudo rm -rf /home/ubuntu/aws-cicd
