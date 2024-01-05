dc4c5262a2a20e09d96b28c59a0d484ffa606075
shesha-sahu <shesha.db@gmail.com>
2024-01-05 18:35:29 +0530

    change in manage.py comment

--- manage.py
+++ manage.py
@@ -13,7 +13,8 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you know "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment?, Note: this change make to reflection for server " 
+        
         ) from exc
     execute_from_command_line(sys.argv)
 
