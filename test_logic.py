6c81fc99a94bab4e6bf8a19a13b1382731577f8e
shesha-sahu <shesha.db@gmail.com>
2024-01-08 16:46:32 +0530

    testcase

--- test_logic.py
+++ test_logic.py
@@ -7,7 +7,7 @@ class MyFunctionTestCase(TestCase):
 
     def test_addition(self):
         result = add(3, 5)
-        self.assertEqual(result, 9, "Incorrect addition result")
+        self.assertEqual(result, 8, "Incorrect addition result")
 
     # def test_substract(self):       
     #     result = sub(10,7)
