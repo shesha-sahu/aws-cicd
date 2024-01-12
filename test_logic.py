e80fadad936b036ef5e5ff85b4124a01051ac723
Shesha Sahu <137204935+shesha-sahu@users.noreply.github.com>
2024-01-13 00:01:19 +0530

    Update test_logic.py

--- test_logic.py
+++ test_logic.py
@@ -6,8 +6,8 @@ from djangoapp.views import add
 class MyFunctionTestCase(TestCase):
 
     def test_addition(self):
-        result = add(3, 5)
-        self.assertEqual(result,9, "Verify additionlll result")
+        result = add(10, 12)
+        self.assertEqual(result,22, "Verify additionlll result")
 
     # def test_substract(self):       
     #     result = sub(10,7)
